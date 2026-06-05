139030835e2da6ecacc89b186fdd3c5a86c4541b
vidhyasagar118 <YOUR_GITHUB_EMAIL>
2026-06-05 16:49:03 +0530

    upload

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://notesweb-backend-9yi6.onrender.com/api",
 });
 
 // Attach token
